bbf8d97e0d85404b8553d03bd6c4d22732c4b5cb
Jpendy <Jpendy256@gmail.com>
2020-05-10 22:41:11 -0700

    thing

--- server.js
+++ server.js
@@ -9,12 +9,14 @@ const app = require('./lib/app');
 
 const PORT = process.env.PORT || 7890;
 
+//get all dogs for full list of dogs page
 app.get('/dogs', async(req, res) => {
   const data = await client.query('SELECT * from dogs'); 
 
   res.json(data.rows);
 });
 
+//get one dog for details page
 app.get('/dogs/:id', async(req, res) => {
   const id = req.params.id;
   const data = await client.query('SELECT * from dogs WHERE id = $1',
@@ -23,6 +25,23 @@ app.get('/dogs/:id', async(req, res) => {
 
   res.json(data.rows);
 });
+
+
+//create new dog entry
+
+app.post('/dogs/', async(req, res) => {
+
+  const data = await client.query(
+    `INSERT INTO dogs (breed, awesomeness_score, owner_id)
+     values ($1, $2, $3)
+     returning *; `,
+    [req.body.breed, req.body.awesomeness_score, 1]
+  );
+
+  res.json(data.rows[0]);
+
+});
+
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
